d29eb940eb7e63a16c13e287ffd0f15c03c86330
Parsa Ahmadi Nasab Emran <parsatempleuniversity@outlook.com>
2025-10-01 12:26:25 -0400

    feat(layout): integrate Footer into App.jsx to display across all pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import ProtectedRoute from './components/ProtectedRoute';
 import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
 import './App.css';
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      <Footer />
     </>
   );
 };
